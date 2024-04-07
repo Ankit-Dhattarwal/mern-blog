64482393b456b29a4283838af1306f7fb00fda5a
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-08 00:24:20 +0530

    remove theme mode reason: not work

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -9,17 +9,17 @@ import {
 } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon, FaSun } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
-import { toggleTheme } from "../redux/theme/themeSlice";
+// import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector } from "react-redux";
+// import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const currentUser = useSelector((state) => state.user.currentUser);
-  const { theme } = useSelector((state) => state.theme);
+  // const { theme } = useSelector((state) => state.theme);
 
   return (
     <Navbar className="border-b-2">
@@ -44,7 +44,7 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button
+        {/* <Button
           className="w-12 h-10 hidden sm:inline"
           color="gray"
           pill
@@ -54,7 +54,7 @@ export default function Header() {
           }}
         >
           {theme === "light" ? <FaSun /> : <FaMoon />}
-        </Button>
+        </Button> */}
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
