5ca82f6268db27095a1685ce9a6479f7275c8388
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-07 16:50:35 +0530

    complete dark mode functionality

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -9,18 +9,23 @@ import {
 } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+
+  const dispatch = useDispatch();
+
   const currentUser = useSelector((state) => state.user.currentUser);
-  console.log("Current User:", currentUser);
+  const { theme } = useSelector((state) => state.theme);
+
   return (
     <Navbar className="border-b-2">
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white text-black"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Coder
@@ -39,8 +44,16 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => {
+            console.log("Toggling theme...");
+            dispatch(toggleTheme());
+          }}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
           <Dropdown
@@ -69,7 +82,6 @@ export default function Header() {
             </Button>
           </Link>
         )}
-
         <NavbarToggle></NavbarToggle>
       </div>
       <Navbar.Collapse>
---
+++ client/src/component/ThemeProvider.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+
+export default function ThemeProvider({ children }) {
+  const { theme } = useSelector((state) => state.theme);
+  console.log(theme);
+  return (
+    <div className={theme}>
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16, 23, 42)] min-h-sceen">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+// import { useSelector } from "react-redux";
+
+// export default function ThemeProvider({ children }) {
+//   const { theme } = useSelector((state) => state.theme);
+
+//   // Define inline styles for background color and text color based on the theme
+//   const styles = {
+//     backgroundColor: theme === "dark" ? "rgb(16, 23, 42)" : "white",
+//     color: theme === "dark" ? "#ccc" : "#333",
+//   };
+
+//   return <div style={styles}>{children}</div>;
+// }
