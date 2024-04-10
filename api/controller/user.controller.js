3fdbe6a2feaff3ad4156bb2c8a97b0755d78a82a
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-11 02:26:54 +0530

    add signout functionality

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -6,6 +6,18 @@ export const test = (req, res) =>{
   res.json({message: 'API is working!'});
 }; 
 
+export const deleteUser = async (req, res , next) => {
+  if(req.user.id != req.params.userId){
+    return next(errorHandler(403, 'You are not allowed to delete this user'))
+  }
+  try{
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted.')
+  }catch(error){
+    next(error);
+  }
+}
+
 export const updateUser = async (req, res, next) => {
  if(req.user.id !== req.params.userId){
   return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -45,3 +57,11 @@ req.body.password = bcryptjs.hashSync(req.body.password, 10);
     next(e);
   }
  };
+
+ export const signout = (req, res, next) => {
+  try{
+    res.clearCookie('access_token').status(200).json('User has been signed Out');
+  } catch(error){
+    next(error);
+  }
+ }
