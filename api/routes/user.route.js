d69d319a232bc41082d1dcb338ed4d9c7ab9e069
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-18 05:23:09 +0530

    add comment section to the post page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {test, updateUser, deleteUser, signout, getUsers} from '../controller/user.controller.js';
+import {test, updateUser, deleteUser, signout, getUsers,} from '../controller/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const routes = express.Router();
