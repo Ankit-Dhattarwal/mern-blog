5ef9b80901d98217e36779ff81da4cf179d8a35c
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-21 10:07:57 +0530

    add delete functionality to the comment components

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
-  createComment, getPostComments, likeComment, editComment
+  createComment, getPostComments, likeComment, editComment, deleteComment
 } from '../controller/comment.controller.js';
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComment/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
