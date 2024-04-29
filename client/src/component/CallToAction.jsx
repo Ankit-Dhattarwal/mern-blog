8d0db98e4cf57d4d4c80d8cbd7fb1c76b2b7d11e
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-30 01:07:23 +0530

    add rock paper scissor project

--- client/src/component/CallToAction.jsx
+++ client/src/component/CallToAction.jsx
@@ -1,26 +1,29 @@
 import { Button } from "flowbite-react";
+import RockPaperScissor from "../assets/RockPaperScissor.png";
 
 export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about Homzy?</h2>
-        <p className="text-gray-500 my-2">Checkout these resources on Github</p>
+        <h2 className="text-2xl">Rock Paper Scissors</h2>
+        <p className="text-gray-500 my-2">
+          Immerse Yourself in the Classic Battle: Rock Paper Scissors Game
+        </p>
         <Button
           gradientDuoTone="purpleToPink"
           className="rounded-tl-xl rounded-bl-none"
         >
           <a
-            href="https://github.com/Ankit-Dhattarwal"
+            href="https://ankit-dhattarwal.github.io/Rock-Paper-Scissors-master/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Homzy
+            Play
           </a>
         </Button>
       </div>
       <div className="p-7 flex-1">
-        <img src="https://uploads-ssl.webflow.com/6447e3efa0bd63d974206f14/644bc3f012ed4b2a5a089f09_hero%20image3.png" />
+        <img src={RockPaperScissor} />
       </div>
     </div>
   );
